b3a71915842dbf430487a88cf9d2c039130428b0
Daria Górecka <dmgorecka@gmail.com>
2021-09-08 08:10:29 +0200

    Adding tags

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,9 +9,11 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addSearchingTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeSearchingTag(tag);
     }
   }
 
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -12,10 +12,14 @@ const createActionName = name => `app/${reducerName}/${name}`;
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
 export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
 export const changeSearchDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const addSearchingTag = payload => ({payload, type: ADD_TAG});
+export const removeSearchingTag = payload => ({payload, type: REMOVE_TAG});
 // reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -34,6 +38,19 @@ export default function reducer(statePart = [], action = {}) {
           [action.payload.type]: parseInt(action.payload.value),
         },
       }
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+       }
+     case REMOVE_TAG:
+       const tags = [...statePart.tags];
+       const index = tags.indexOf(action.payload);
+       tags.splice(index, 1);
+       return {
+         ...statePart,
+         tags,
+       }
     default:
       return statePart;
   }
