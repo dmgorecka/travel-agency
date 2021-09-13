c2d1d086c81636208e61f1c89c44e418ead565b1
Daria Górecka <dmgorecka@gmail.com>
2021-09-13 09:31:30 +0200

    Add Checkboxes, Icons, Dropdown and Number

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,12 +5,12 @@ import PropTypes from 'prop-types';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
 
-const OrderForm = ({tripCost, options}) => {
+const OrderForm = ({tripCost, options, setOrderOption}) => {
     return (
         <Row>
             {pricing.map(opt => (
                 <Col md={4} key={opt.id}>
-                    <OrderOption {...opt}/>
+                    <OrderOption {...opt} currentValue={options[opt.id]} setOrderOption={setOrderOption}/>
                 </Col>
             ))}
             <Col xs={12}>
@@ -26,7 +26,7 @@ const OrderForm = ({tripCost, options}) => {
 OrderForm.propTypes = {
     tripCost: PropTypes.string,
     options: PropTypes.object,
-
+    setOrderOption: PropTypes.func,
 }
 
 export default OrderForm;
--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from './OrderOption.module.scss';
+import PropTypes from 'prop-types';
 
-const OrderOptionNumber = () => (
-    <div>OrderOptionNumber</div>
-);
+const OrderOptionNumber = ({currentValue, setOptionValue, limits}) => (
+    <div className={styles.number}>
+      <input
+        type='number'
+        value={currentValue}
+        min={limits.min}
+        max={limits.max}
+        onChange={event => setOptionValue(event.currentTarget.value)}
+      />
+    </div>
+  );
+
+  OrderOptionNumber.propTypes = {
+    currentValue: PropTypes.node,
+    setOptionValue: PropTypes.func,
+    limits: PropTypes.object,
+  };
 
 export default OrderOptionNumber;
